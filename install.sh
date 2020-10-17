b6c4f51559a9d1e0eed16d48dacd65cb854d25c3
acintosh <g1731057@m.sojo-u.ac.jp>
2020-10-17 12:25:03 +0900

    fix some typo

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+ROOT_DIR="$(dirname $0)"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 mkdir -p "${XDG_CONFIG_HOME}/mpd"
 mkdir -p "${XDG_CONFIG_HOME}/systemd"
-cp -r ./mpd/* "${XDG_CONFIG_HOME}/mpd/"
-cp -r ./systemd/* "${XDG_CONFIG_HOME}/systemd/"
+cp -r "${ROOT_DIR}"/mpd/* "${XDG_CONFIG_HOME}/mpd/"
+cp -r "${ROOT_DIR}"/systemd/* "${XDG_CONFIG_HOME}/systemd/"
 
 systemctl --user daemon-reload
 systemctl --user start mpd
